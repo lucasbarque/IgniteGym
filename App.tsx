453a46b569a0ff3bc661002dc25962332911632a
Lucas Barque <lucasbarquedasilva@hotmail.com>
2023-04-07 07:39:40 -0400

    Routes

--- App.tsx
+++ App.tsx
@@ -10,8 +10,7 @@ import {
 import { Loading } from "@components/Loading";
 
 import { THEME } from "./src/theme";
-import { SignIn } from "@screens/SignIn";
-import { SignUp } from "@screens/SignUp";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -23,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
