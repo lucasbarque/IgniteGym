105371af13c3a0fcc94c2a2061edb0b2aadb6e5d
Lucas Barque <lucasbarquedasilva@hotmail.com>
2023-04-07 07:24:51 -0400

    SignUp

--- App.tsx
+++ App.tsx
@@ -11,6 +11,7 @@ import { Loading } from "@components/Loading";
 
 import { THEME } from "./src/theme";
 import { SignIn } from "@screens/SignIn";
+import { SignUp } from "@screens/SignUp";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -22,7 +23,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <SignUp /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -5,7 +5,7 @@ import LogoSvg from "@assets/logo.svg";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
-export function SignIn() {
+export function SignUp() {
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -28,8 +28,10 @@ export function SignIn() {
 
         <Center>
           <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
-            Acesse sua conta
+            Crie sua conta
           </Heading>
+
+          <Input placeholder="Nome" />
 
           <Input
             placeholder="E-mail"
@@ -39,16 +41,10 @@ export function SignIn() {
 
           <Input placeholder="Senha" secureTextEntry />
 
-          <Button title="Acessar" />
+          <Button title="Criar e acessar" />
         </Center>
 
-        <Center mt={24}>
-          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
-            Ainda não tem acesso?
-          </Text>
-
-          <Button title="Criar conta" variant="outline" />
-        </Center>
+        <Button mt={24} title="Voltar para o login" variant="outline" />
       </VStack>
     </ScrollView>
   );
